ef8b05b10c6fd23f77c6c78517c131b69cb973d7
Marko Gargenta <marko@marakana.com>
2013-07-22 12:30:10 -0700

    Final version

---
+++ Yamba/src/com/oscon/yamba/MainActivity.java
@@ -0,0 +1,37 @@
+package com.oscon.yamba;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_update:
+			startActivity( new Intent(this, StatusActivity.class) );
+			return true;
+		case R.id.action_settings:
+			// TODO
+			return true;
+		default:
+			return false;
+		}
+	}
+}

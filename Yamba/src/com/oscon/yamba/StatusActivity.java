ba276d33d5267839a44adb972d02d56cf463d82c
Marko Gargenta <marko@marakana.com>
2013-07-22 11:05:45 -0700

    Implemented event handlin

--- Yamba/src/com/oscon/yamba/StatusActivity.java
+++ Yamba/src/com/oscon/yamba/StatusActivity.java
@@ -2,14 +2,30 @@ package com.oscon.yamba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class StatusActivity extends Activity {
+	private Button buttonUpdate;
+	private EditText editStatus;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
 
+		editStatus = (EditText) findViewById(R.id.edit_status);
+		buttonUpdate = (Button) findViewById(R.id.button_update);
+		buttonUpdate.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Log.d("StatusActivity","onClicked!");
+			}
+		});
+	}
 
 }

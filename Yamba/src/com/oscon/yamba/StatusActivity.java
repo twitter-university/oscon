a1229db93f8d96ec633ccdc7c736db70d271f07c
Marko Gargenta <marko@marakana.com>
2013-07-22 11:45:39 -0700

    Basic AsyncTask

--- Yamba/src/com/oscon/yamba/StatusActivity.java
+++ Yamba/src/com/oscon/yamba/StatusActivity.java
@@ -1,6 +1,7 @@
 package com.oscon.yamba;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,7 +9,11 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
 public class StatusActivity extends Activity {
+	private static final String TAG = "StatusActivity";
 	private Button buttonUpdate;
 	private EditText editStatus;
 
@@ -24,9 +29,29 @@ public class StatusActivity extends Activity {
 			@Override
 			public void onClick(View view) {
 				String status = editStatus.getText().toString();
-				Log.d("StatusActivity","onClicked with status: "+status);
+				Log.d(TAG,"onClicked with status: "+status);
+				
+				new PostTask().execute(status);
 			}
 		});
+	}
+	
+	private static class PostTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			YambaClient yamba = new YambaClient("student","password");
+			try {
+				yamba.postStatus(params[0]);
+				
+				return "Successfully posted!";
+			} catch (YambaClientException e) {
+				Log.e(TAG, "Failed to post", e);
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		
 	}
 
 }
